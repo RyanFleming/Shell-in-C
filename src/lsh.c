56760263afda1dfa52b633b1a2d5eab05ed01937
RyanFleming <fleminry@oregonstate.edu>
2017-08-30 12:22:02 -0700

    Basic Loop of a Shell
    Read, Parse, Execute

--- src/lsh.c
+++ src/lsh.c
@@ -5,6 +5,32 @@
 // Demonstrates the basics of how a shell works including:
 // read, parse, fork, exec, and wait.
 //
+#include <sys/wait.h>
+#include <unistd.h>
+#include <stdlib.h>
+#include <stdio.h>
+#include <string.h>
+//
+// Description Loop getting input and executing it.
+//
+void LshLoop(void) {
+  char *line;
+  char **args;
+  int status;
+  do {
+    printf("> ");
+    line = LshReadLine();
+    args = LshSplitLine(line);
+    status = LshExecute(args);
+    free(line);
+    free(args);
+  } while(status);
+}
+//
+// Description: Main entry point.
+// Parameter: agrc Argument count
+// Parameter: argv Argument vector
+// Return: status code
 int main(int argc, char **argv) {
   //Load config files, if any.
   //Run command loop.
