7850728b41bf4ee51b934937b425807daa6778e6
RyanFleming <ryanwfleming@gmail.com>
2017-08-30 16:02:06 -0700

    update LshLaunch function to fork child process and launch the program

--- src/lsh.c
+++ src/lsh.c
@@ -10,6 +10,32 @@
 #include <stdlib.h>
 #include <stdio.h>
 #include <string.h>
+//
+// Description: Launch a program and wait for it to terminate.
+// Paramter: args Null terminated list of arguments (including program).
+// Return: Always returns to 1, to continue execution.
+//
+int LshLaunch(char **args) {
+  pid_t pid;
+  int status;
+  pid = fork();
+  if (pid == 0) {
+    //Child process
+    if (execvp(args[0], args) == -1) {
+      perror("lsh");
+    }
+    exit(EXIT_FAILURE);
+  } else if (pid < 0) {
+    //Error forking
+    perror("lsh");
+  } else {
+    //Parent process
+    do {
+      waitpid(pid, &status, WUNTRACED);
+    } while (!WIFEXITED(status) && !WIFSIGNALED(status));
+  }
+  return 1;
+}
 #define LSH_RL_BUFSIZE 1024
 //
 // Description: Read a line of input from stdin
